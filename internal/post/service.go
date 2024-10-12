da601c42534bbf70623a73930dd29515283804f1
NewHouses <sergio.simons.c@gmail.com>
2024-10-12 17:22:29 +0200

    [OSP-56] Removing unnecessary fily extension information

--- internal/post/service.go
+++ internal/post/service.go
@@ -22,7 +22,6 @@ type PostMetadata struct {
 	PostId      string    `json:"post_id"`
 	Username    string    `json:"username"`
 	Type        string    `json:"type"`
-	FileType    string    `json:"file_type"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
