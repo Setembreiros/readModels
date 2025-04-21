423216e93c09ed138d243b8a20febd2cbb0c1288
NewHouses <serxio.simons.casasnovas@gmail.com>
2025-04-21 17:38:25 +0200

    [OSP-94] Adding service

---
+++ internal/reaction/test/unit_test/service_test.go
@@ -0,0 +1,46 @@
+package reaction_test
+
+import (
+	"errors"
+	"readmodels/internal/model"
+	"readmodels/internal/reaction"
+	mock_reaction "readmodels/internal/reaction/test/mock"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var repositoryService *mock_reaction.MockRepository
+var reactionService *reaction.ReactionService
+
+func setUpService(t *testing.T) {
+	SetUp(t)
+	repositoryService = mock_reaction.NewMockRepository(ctrl)
+	reactionService = reaction.NewCommentService(repositoryService)
+}
+
+func TestCreateLikePostWithService(t *testing.T) {
+	setUpService(t)
+	data := &model.LikePost{
+		Username: "user123",
+		PostId:   "post123",
+	}
+	repositoryService.EXPECT().CreateLikePost(data)
+
+	reactionService.CreateLikePost(data)
+
+	assert.Contains(t, loggerOutput.String(), "Like was created, username: user123 -> postId: post123")
+}
+
+func TestErrorOnCreateNewCommentWithService(t *testing.T) {
+	setUpService(t)
+	data := &model.LikePost{
+		Username: "user123",
+		PostId:   "post123",
+	}
+	repositoryService.EXPECT().CreateLikePost(data).Return(errors.New("some error"))
+
+	reactionService.CreateLikePost(data)
+
+	assert.Contains(t, loggerOutput.String(), "Error creating like, username: user123 -> postId: post123")
+}
